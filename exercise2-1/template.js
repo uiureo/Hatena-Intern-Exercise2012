773af4ee2559e2aa2007fd9811bda23267b07e80
Kazato Sugimoto <uiureo@gmail.com>
2012-08-06 16:28:29 +0900

    課題2-1をやった

--- exercise2-1/template.js
+++ exercise2-1/template.js
@@ -1,9 +1,31 @@
 var Template = function(input) {
-    // この関数を実装してください
+  if (input.source === null || input.source === undefined || typeof input.source !== 'string') {
+    throw {
+      name: 'TypeError',
+      message: 'source must be a string'
+    };
+  }
+  this.source = input.source;
 };
 
 Template.prototype = {
     render: function(variables) {
-        // この関数を実装してください
+      var escapeHTML = function (text) {
+        var character = {
+          '<' : '&lt;',
+          '>' : '&gt;',
+          '&' : '&amp;',
+          '"' : '&quot;'
+        };
+
+        return text.replace(/[<>&"]/g, function (c) {
+          return character[c];
+        });
+      };
+
+      return this.source.replace(/{%\s*(\w+)\s*%}/g, function (_, name) {
+        return escapeHTML(variables[name]);
+      });
+
     }
 };
